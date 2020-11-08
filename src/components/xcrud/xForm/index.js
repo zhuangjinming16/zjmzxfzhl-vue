b1e0bd7ed9f071ed27c324e3c97e65aabf12b21f
zjm16@163.com <zjm16@163.com>
2020-11-08 19:38:33 +0800

    delete old bpmn panel

--- src/components/xcrud/xForm/index.js
+++
@@ -1,7 +0,0 @@
-import xForm from './src/xForm'
-
-xForm.install = function(Vue) {
-  Vue.component(xForm.name, xForm)
-}
-
-export default xForm
--- src/components/xcrud/xTable/index.js
+++
@@ -1,7 +0,0 @@
-import xTable from './src/xTable'
-
-xTable.install = function(Vue) {
-  Vue.component(xTable.name, xTable)
-}
-
-export default xTable
